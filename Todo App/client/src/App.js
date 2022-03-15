f762119802d7c73189c35671f3317f93137bc9fe
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-03-15 11:54:43 +0530

    Initial Commit

--- Todo App/client/src/App.js
+++ Todo App/client/src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <Fragment></Fragment>;
 }
 
 export default App;
