4d74d8069f55f14829e8aa82f16a4526e10d0122
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-03-15 13:55:45 +0530

    Update App.js

--- Todo App/client/src/App.js
+++ Todo App/client/src/App.js
@@ -1,8 +1,19 @@
 import React, { Fragment } from "react";
 import "./App.css";
 
+// Components
+import InputTodo from "./components/InputTodo";
+import ListTodo from "./components/ListTodos";
+
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        <ListTodo />
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
