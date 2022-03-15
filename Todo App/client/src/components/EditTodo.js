26c068c6440d15840b9f1063b6de969aa80de5f6
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2022-03-15 13:55:50 +0530

    Update EditTodo.js

---
+++ Todo App/client/src/components/EditTodo.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useState, useEffect } from "react";
+
+const EditTodo = () => {
+  return (
+    <Fragment className="container">
+      <button
+        type="button"
+        className="btn btn-warning"
+        data-toggle="modal"
+        data-target="#myModal"
+      >
+        Edit
+      </button>
+
+      <div className="modal" id="myModal">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Todo</h4>
+              <button type="button" className="close" data-dismiss="modal">
+                &times;
+              </button>
+            </div>
+
+            <div className="modal-body"><input type="text" className="form-control" /></div>
+
+            <div className="modal-footer">
+              <button type="button" className="btn btn-warning" data-dismiss="modal">
+                Edit
+              </button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal">
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default EditTodo;
